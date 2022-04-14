4acd0f7eb5b64ab7a1e68ea4444fb3faa5b7ecd8
mshangle1 <mshangle207@gmail.com>
2022-04-14 17:54:13 -0400

    Initial commit

---
+++ src/main/java/com/mshangle/storyboard/storyboard/StoryboardApplication.java
@@ -0,0 +1,13 @@
+package com.mshangle.storyboard.storyboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoryboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoryboardApplication.class, args);
+	}
+
+}
