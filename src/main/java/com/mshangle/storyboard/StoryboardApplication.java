2216980f929dae5300f981095a4eb47af0c75c77
mshangle1 <mshangle207@gmail.com>
2022-04-14 18:04:29 -0400

    Renamed root project and fixed project structure

--- src/main/java/com/mshangle/storyboard/StoryboardApplication.java
+++ src/main/java/com/mshangle/storyboard/StoryboardApplication.java
@@ -1,4 +1,4 @@
-package com.mshangle.storyboard.storyboard;
+package com.mshangle.storyboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
